b435c712ba2111c1ef59a3669dbba679e178beef
tikhonova.t@pm.me <tikhonova.t@pm.me>
2018-08-16 12:45:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Tatiana"
-var height = "169"
+var height = 169
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+1)
+console.log(height+1)
